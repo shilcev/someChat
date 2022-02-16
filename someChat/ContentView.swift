bb9d886c440e1533fd8afad9a13fb29fb96acfe4
evgenii shiltsev <e.shiltsev@gmail.com>
2022-02-16 15:20:39 +0200

    Initial Commit

---
+++ someChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  someChat
+//
+//  Created by Evgenii Shiltsev on 16.2.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ someChat/someChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  someChatApp.swift
+//  someChat
+//
+//  Created by Evgenii Shiltsev on 16.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct someChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
