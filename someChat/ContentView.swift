dfdb75ee9be2e3f62026672d4c00c1782649a3bb
evgenii shiltsev <e.shiltsev@gmail.com>
2022-02-16 15:32:06 +0200

    create

--- someChat/ContentView.swift
+++ someChat/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color.purple
+        .ignoresSafeArea()
+            
+        Text("Botton here")
             .padding()
+            .foregroundColor(.black)
+            .shadow(color: .black, radius: 1, x: 1, y: 1)
+            
+        }
     }
 }
 
